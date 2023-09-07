3228d27b657bf61885511717226511ee680a0c8d
mouaa <mouaa@DESKTOP-NT2AJPK>
2023-09-07 17:37:49 -0400

    Add project files.

---
+++ 20230907_mouaAngelina_Exercise2.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveEstimator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal baseCost = 200.00m;
+            Console.WriteLine("How many hours will you expect to need for the job?");
+            decimal hours = Convert.ToDecimal(Console.ReadLine());
+            decimal totalHours = hours * 150;
+            Console.WriteLine("How many miles do you expect to move?");
+            decimal miles = Convert.ToInt32(Console.ReadLine());
+            decimal totalMiles = (miles * 2);
+            decimal totalEstimate = (baseCost + totalHours + totalMiles);
+            Console.WriteLine("Your estimate for this move is $" + totalEstimate);
+            Console.ReadLine();
+        }
+    }
+}
